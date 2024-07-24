1abdc8af33dc5785f750692e9433a06eb8ed1305
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2024-07-24 17:34:47 +0200

    test: update SyncAgent.kt to test the automatic replay

--- jakta-dsl/src/test/kotlin/it/unibo/jakta/agents/bdi/dsl/SyncAgent.kt
+++ jakta-dsl/src/test/kotlin/it/unibo/jakta/agents/bdi/dsl/SyncAgent.kt
@@ -1,5 +1,9 @@
 package it.unibo.jakta.agents.bdi.dsl
 
+import it.unibo.tuprolog.core.Atom
+import it.unibo.tuprolog.core.Term
+import it.unibo.tuprolog.unify.Unificator.Companion.mguWith
+
 fun main() {
     mas {
         agent("sender") {
@@ -8,17 +12,25 @@ fun main() {
             }
             plans {
                 +achieve("ask"("question")) then {
+                    execute("print"("asking a question..."))
                     execute("send"("receiver", "askOne", "good"(X)))
-                    execute("print"(X))
+                    execute("print"("the answer is ", X))
                 }
             }
         }
         agent("receiver") {
+            actions {
+                action("respond", 1) {
+                    val arg: Term = argument(0)
+                    addResults(arg mguWith Atom.of("ice-cream"))
+                }
+            }
             plans {
                 +test("good"("source"(S), Y)) then {
-                    execute("send"(S, "tell", "good"("ice-cream")))
+                    execute("print"("responding..."))
+                    execute("respond"(Y))
                 }
             }
         }
-    }.start()
+    }.start(true)
 }
